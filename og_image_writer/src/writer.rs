8864b5a2b8051a5fdd20651e98a44c3348035277
keiya01 <keiya.s.0210@gmail.com>
2021-08-30 22:53:13 +0900

    fix: refactor and fix some bugs

--- og_image_writer/src/writer.rs
+++ og_image_writer/src/writer.rs
@@ -1,85 +1,7 @@
 use cairo::{ImageSurface, Context, Format};
-use std::{fs::File, io, ops::Range, str};
+use std::{fs::File, io, str};
 use super::style::{WordBreak, Style, WindowStyle};
-
-struct LineBreaker<'a> {
-    title: &'a str,
-    lines: Vec<Range<usize>>,
-}
-
-// TODO: support truncate text when overflow specified height.
-impl<'a> LineBreaker<'a> {
-    fn new(title: &'a str) -> Self {
-        LineBreaker {
-            title,
-            lines: vec![],
-        }
-    }
-
-    // TODO: support hyphenation
-    fn break_text_with_whitespace(&mut self, context: &Context, width: f64) {
-        let text_arr: Vec<&str> = self.title.split_whitespace().collect();
-
-        let text_arr_len = text_arr.len();
-
-        let whitespace_width = context.text_extents(" ").unwrap().x_advance;
-        let whitespace_idx = 1;
-
-        let mut line = 0..0;
-        let mut line_width = 0.;
-        for (i, text) in text_arr.into_iter().enumerate() {
-            let extents = context.text_extents(text).unwrap();
-
-            let is_last = text_arr_len - 1 == i;
-
-            let text_width = extents.width;
-            let text_width = if is_last {
-                text_width
-            } else {
-                text_width + whitespace_width
-            };
-
-            if width <= line_width + text_width {
-                let start = line.end;
-                self.lines.push(line);
-                line = start..start;
-                line_width = 0.;
-            }
-
-            line.end += text.len() + whitespace_idx;
-            line_width += text_width;
-        }
-
-        // End of line should not have whitespace
-        line.end -= whitespace_idx;
-
-        self.lines.push(line);
-    }
-
-    fn break_text_with_char(&mut self, context: &Context, width: f64) {
-        let chars = self.title.char_indices();
-
-        let mut line = 0..0;
-        let mut line_width = 0.;
-        for (i, ch) in chars.into_iter() {
-            let extents = context.text_extents(&ch.to_string()).unwrap();
-
-            let ch_width = extents.x_advance;
-
-            if width <= line_width + ch_width {
-                let start = line.end;
-                self.lines.push(line);
-                line = start..start;
-                line_width = 0.;
-            }
-
-            line.end = i;
-            line_width += ch_width;
-        }
-
-        self.lines.push(line);
-    }
-}
+use super::line_breaker::{LineBreaker};
 
 #[derive(Default)]
 struct StyleSheet<'a> {
@@ -177,12 +99,12 @@ impl<'a> OGImageWriter<'a> {
         let text_area_width = window_width - style.margin_inline * 2.;
 
         // Initialize font metrics for line breaking.
-        set_font(&context, style);
+        set_font(context, style);
 
         let mut line_breaker = LineBreaker::new(self.text);
         match style.word_break {
-            WordBreak::Normal => line_breaker.break_text_with_whitespace(&context, text_area_width),
-            WordBreak::BreakAll => line_breaker.break_text_with_char(&context, text_area_width),
+            WordBreak::Normal => line_breaker.break_text_with_whitespace(context, text_area_width),
+            WordBreak::BreakAll => line_breaker.break_text_with_char(context, text_area_width),
         }
 
         let mut total_height = 0.;
@@ -196,16 +118,22 @@ impl<'a> OGImageWriter<'a> {
         for (i, line) in line_breaker.lines.into_iter().enumerate() {
             let is_first_line = i == 0;
 
-            set_font(&context, style);
+            set_font(context, style);
 
             let text = &self.text[line.clone()];
 
             let extents = context.text_extents(text).unwrap();
             let text_height = extents.height;
 
+            if lines_len == 1 {
+                context.move_to(window_width / 2. - extents.width / 2., window_height / 2.);
+                context.show_text(text).unwrap();
+                break;
+            }
+
             let line_height = text_height * style.line_height / 2.;
 
-            let pos_y = ((window_height - total_height) / 2.) - text_height / 2. + prev_extents_height;
+            let pos_y = ((window_height - total_height) / 2.) + prev_extents_height;
             let pos_y = if !is_first_line {
                 pos_y + line_height
             } else {
@@ -217,12 +145,6 @@ impl<'a> OGImageWriter<'a> {
             } else {
                 text_height
             };
-
-            if lines_len == 1 {
-                context.move_to(window_width / 2. - extents.width / 2., window_height / 2. - text_height / 2.);
-                context.show_text(text).unwrap();
-                break;
-            }
 
             context.move_to(style.margin_inline, pos_y);
             context.show_text(text).unwrap();
