c3ba8fe28481a3ec7f975ce05657aa9185ab12ca
keiya01 <keiya.s.0210@gmail.com>
2021-08-31 22:01:41 +0900

    fix: fix example

--- examples/background_image.rs
+++ examples/background_image.rs
@@ -10,7 +10,7 @@ fn main() -> io::Result<()> {
         width: 1024,
         height: 512,
         background_image: Some("./examples/assets/og_template.png"),
-        align_items: style::AlignItems::End,
+        align_items: style::AlignItems::Center,
         justify_content: style::JustifyContent::Center,
         ..style::WindowStyle::default()
     });
@@ -25,7 +25,7 @@ fn main() -> io::Result<()> {
         font_weight: style::FontWeight::Bold,
         word_break: style::WordBreak::Normal,
         color: style::RGB(1., 1., 1.),
-        text_align: style::TextAlign::Center,
+        text_align: style::TextAlign::Start,
     });
 
     let out_dir = "./examples/dist";
