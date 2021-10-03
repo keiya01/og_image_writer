1fa59131e1a346cf06db41d1bf2f0ed599de4a27
keiya01 <keiya.s.0210@gmail.com>
2021-10-03 15:13:13 +0900

    example: add example for textarea

---
+++ examples/textarea.rs
@@ -0,0 +1,61 @@
+use og_image_writer::{style, writer::OGImageWriter, TextArea};
+use std::path::Path;
+
+fn main() -> anyhow::Result<()> {
+    let mut writer = OGImageWriter::new(style::WindowStyle {
+        width: 1024,
+        height: 512,
+        background_color: Some(style::Rgba([70, 40, 90, 255])),
+        align_items: style::AlignItems::Center,
+        justify_content: style::JustifyContent::Center,
+        ..style::WindowStyle::default()
+    })?;
+
+    let font = Vec::from(include_bytes!("../fonts/Mplus1-Black.ttf") as &[u8]);
+
+    let mut textarea = TextArea::new();
+    textarea.push_text("This is ");
+    textarea.push(
+        "Open Graphic Image Writer",
+        style::Style {
+            color: style::Rgba([255, 0, 255, 255]),
+            font_size: 100.,
+            ..style::Style::default()
+        },
+        None,
+    )?;
+    textarea.push_text(" for ");
+    textarea.push(
+        "Web Developer!!!!!",
+        style::Style {
+            color: style::Rgba([255, 0, 0, 255]),
+            font_size: 100.,
+            ..style::Style::default()
+        },
+        None,
+    )?;
+
+    writer.set_textarea(
+        textarea,
+        style::Style {
+            margin: style::Margin(0, 20, 0, 20),
+            line_height: 1.8,
+            font_size: 100.,
+            // word_break: style::WordBreak::Normal,
+            color: style::Rgba([255, 255, 255, 255]),
+            text_align: style::TextAlign::Start,
+            max_height: Some(150),
+            text_overflow: style::TextOverflow::Ellipsis,
+            word_break: style::WordBreak::BreakAll,
+            ..style::Style::default()
+        },
+        font,
+    )?;
+
+    let out_dir = "./examples/assets";
+    let out_filename = "output_textarea.png";
+
+    writer.generate(Path::new(&format!("{}/{}", out_dir, out_filename)))?;
+
+    Ok(())
+}
