fe2b3ed63581020d276a581c3d610de674ff53ff
keiya01 <keiya.s.0210@gmail.com>
2021-11-12 08:59:26 +0900

    feat: add github icon on header

--- docs/src/lib/Header/Header.svelte
+++ docs/src/lib/Header/Header.svelte
@@ -1,9 +1,19 @@
+<script lang="ts">
+  import github from '../../assets/github.png?url';
+</script>
+
 <header>
   <h1>Playground</h1>
+  <a href="https://github.com/keiya01/og_image_writer">
+    <img src={github} alt="GitHub" height={120} width={120} />
+  </a>
 </header>
 
 <style>
   header {
+    align-items: center;
+    display: flex;
+    grid-template-rows: 1fr 60px;
     width: 100%;
     padding: 5px 10px;
     box-sizing: border-box;
@@ -12,5 +22,12 @@
   h1 {
     color: var(--main-color);
     margin: 0;
+    flex: 1;
+  }
+
+  img {
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
   }
 </style>
