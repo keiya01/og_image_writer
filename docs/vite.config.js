4c51cb232b47389bb050c7f4061e27f4a480faab
keiya01 <keiya.s.0210@gmail.com>
2021-11-14 16:41:08 +0900

    fix: remove rollup-copy-plugin

--- docs/vite.config.js
+++ docs/vite.config.js
@@ -1,20 +1,7 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
-import copy from "rollup-plugin-copy";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    svelte(),
-    copy({
-      targets: [
-        {
-          src: "node_modules/og_image_writer/wasm_bg.wasm",
-          dest: "dist",
-        },
-      ],
-      verbose: true,
-      hook: "writeBundle",
-    }),
-  ],
+  plugins: [svelte()],
 });
