bb4456973f3e019af9035e962c911b46ec96a7d5
keiya01 <keiya.s.0210@gmail.com>
2021-12-04 11:38:28 +0900

    build: fix vite build config

--- docs/vite.config.js
+++ docs/vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from "vite";
+import path from "path";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
+
+const IS_PROD = process.env.NODE_ENV === "production";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  resolve: {
+    alias: !IS_PROD
+      ? {
+          og_image_writer: path.resolve("../wasm/pkg"),
+        }
+      : undefined,
+  },
 });
