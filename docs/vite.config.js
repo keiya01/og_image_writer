945b74d3cad94797dd680e98d1502b3394aaa9ff
keiya01 <keiya.s.0210@gmail.com>
2021-12-04 12:55:00 +0900

    build: fix vite config

--- docs/vite.config.js
+++ docs/vite.config.js
@@ -1,12 +1,17 @@
 import { defineConfig } from "vite";
 import path from "path";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
+import autoPreprocess from "svelte-preprocess";
 
 const IS_PROD = process.env.NODE_ENV === "production";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [
+    svelte({
+      preprocess: autoPreprocess(),
+    }),
+  ],
   resolve: {
     alias: !IS_PROD
       ? {
