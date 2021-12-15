957b068d0cb3d5d51b5cde3e72f7c88ae5a4cf9d
keiya01 <keiya.s.0210@gmail.com>
2021-12-15 12:27:27 +0900

    build: fix build config

--- wasm/script/build.sh
+++ wasm/script/build.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
-wasm-pack build --target web --release
+wasm-pack build --release --target web
 sed -i "" -e 's/"name": "wasm"/"name": "og_image_writer"/' pkg/package.json
+wasm-strip pkg/wasm_bg.wasm
